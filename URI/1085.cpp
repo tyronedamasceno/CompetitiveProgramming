23e80dd087f85e969d89134989c083d48281da22
Tyrone Damasceno <tyronedamasceno@gmail.com>
2016-09-17 10:35:36 -0300

    Dijsktra modificado - TODO

---
+++ URI/1085.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef pair <int, int> ii;
+typedef vector <ii> vii;
+typedef map <int, int> mii;
+typedef map <string, int> msi;
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define clr(a, k) memset(a, k, sizeof(a))
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define in1(a) scanf("%d", &a)
+#define in2(a, b) scanf("%d %d", &a, &b)
+#define in3(a, b, c) scanf("%d %d %d", &a, &b, &c)
+
+
+
+
+int main()
+{
+	
+
+
+	return 0;
+}
