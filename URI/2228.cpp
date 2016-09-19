7f174667bbde3762099360db3c7a5494e2d84813
Tyrone Damasceno <tyronedamasceno@gmail.com>
2016-09-19 09:23:56 -0300

    Accepted with SubSet Sum problem solution

--- URI/2228.cpp
+++ URI/2228.cpp
@@ -19,34 +19,54 @@ typedef map <string, int> msi;
 #define in2(a, b) scanf("%d %d", &a, &b)
 #define in3(a, b, c) scanf("%d %d %d", &a, &b, &c)
 
+bool dp[10001][101];
+int joias[101];
+
+bool subset_sum(int v[], int n, int t) {
+    int i, j;
+    
+    for (j = 0; j <= n; j++) dp[0][j] = true;
+    for (i = 1; i <= t; i++) dp[i][0] = false;
+   
+    for (j = 1; j <= n; j++)
+        for (i = 1; i <= t; i++) {
+            dp[i][j] = dp[i][j-1];
+            if ( i >= v[j] && dp[i-v[j]][j-1] )
+                dp[i][j] = true;
+        }
+
+    return dp[t][n];
+}
+
+
 int main()
 {
-	int x, y, n, aux, t = 0;
-	vi v;
 
-	while (cin >> x >> y >> n && (x && y && n))
-	{
-		v.clear();
-		rep(i, 0, n)
-		{
-			cin >> aux;
-			v.pb(aux);
-		}
-		sort(v.rbegin(), v.rend());
+    int n, i, x, y, sum, t = 1;
 
-		rep(i, 0, n)
-		{
-			if (x < y) x += v[i];
-			else y += v[i];
-		}
-
-		cout << "Teste " << ++t << endl;
-		if (x == y) cout << "S" << endl;
-		else cout << "N" << endl;
-
-		cout << endl;
-
-	}
-
-	return 0;
+    while (true)
+    {
+        in3(x, y, n);
+        
+        if ( x == 0 && y == 0 && n == 0 )
+            break;
+            
+        for (sum = 0, i = 1; i <= n; i++) 
+        {
+            in1(joias[i]);
+            sum += joias[i];
+        }
+            
+        printf("Teste %d\n", t++);
+        if ( (x + y + sum) % 2 || min(x, y) + sum < max(x, y) ) 
+        {
+            printf("N\n\n");
+            continue;
+        }
+        
+        printf("%c\n\n", subset_sum(joias, n, (x+y+sum)/2 - x) ? 'S' : 'N');
+        
+    }
+    
+    return 0;
 }
